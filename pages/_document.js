d6e6f2e3d1587976329748dfbb0fbfe4f1835a74
Stef Kors <stef@Stefs-MacBook-Air.local>
2019-04-14 12:13:31 +0200

    add fathom

--- pages/_document.js
+++ pages/_document.js
@@ -3,6 +3,7 @@
 
 // ./pages/_document.js
 import Document, { Head, Main, NextScript } from 'next/document'
+import Safe from 'react-safe'
 
 export default class MyDocument extends Document {
   static async getInitialProps(ctx) {
@@ -17,24 +18,25 @@ export default class MyDocument extends Document {
           <style>{`
           body { margin: 0; font-size: 14px; } *, *:before, *:after {box-sizing: border-box;} /* custom! */
           `}</style>
+          <Safe.script>{
+            `(function(f, a, t, h, o, m){
+              a[h]=a[h]||function(){
+                (a[h].q=a[h].q||[]).push(arguments)
+              };
+              o=f.createElement('script'),
+              m=f.getElementsByTagName('script')[0];
+              o.async=1; o.src=t; o.id='fathom-script';
+              m.parentNode.insertBefore(o,m)
+            })(document, window, '//analytics.stefkors.nl/tracker.js', 'fathom');
+            fathom('set', 'siteId', 'USTQN');
+            fathom('trackPageview');`
+          }
+          </Safe.script>
         </Head>
         <body>
           <Main />
           <NextScript />
         </body>
-        <script>
-        (function(f, a, t, h, o, m){
-          a[h]=a[h]||function(){
-            (a[h].q=a[h].q||[]).push(arguments)
-          };
-          o=f.createElement('script'),
-          m=f.getElementsByTagName('script')[0];
-          o.async=1; o.src=t; o.id='fathom-script';
-          m.parentNode.insertBefore(o,m)
-        })(document, window, '//analytics.stefkors.nl/tracker.js', 'fathom');
-        fathom('set', 'siteId', 'USTQN');
-        fathom('trackPageview');
-        </script>
       </html>
     )
   }
